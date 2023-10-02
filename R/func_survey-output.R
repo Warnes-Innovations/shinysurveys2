3313d80ad73090878ac566c60175adbc2dd3bd21
Anudeep Reddy Banda <37535240+anudeepreddy36@users.noreply.github.com>
2023-10-02 12:22:50 -0400

    Update func_survey-output.R

--- R/func_survey-output.R
+++ R/func_survey-output.R
@@ -129,7 +129,7 @@ surveyOutput_individual <- function(df) {
                                        selected = NULL,
                                        choiceNames = NULL,
                                        choiceValues = NULL,
-                                       rowIDsName="Grade",
+                                       rowIDsName="",
                                        labelsWidth = list(NULL, NULL))
 
     output <- shiny::tagList(
